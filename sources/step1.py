1ec732f56050734a0a1e587fbf6e79c227c44d5a
Brad Neil <brad@friedorange.xyz>
2022-07-22 17:18:57 +1000

    Change glyph map to AGL names

--- sources/step1.py
+++ sources/step1.py
@@ -80,7 +80,7 @@ def main():
 						if source_image[j*glyph_height + y][i*glyph_width + x]:
 							font[name].addReference("dot", (1, 0, 0, 1, x * DOT_SIZE, (glyph_height - y - HEADROOM) * DOT_SIZE))
 
-			font[name].width = glyph_width * DOT_SIZE
+				font[name].width = glyph_width * DOT_SIZE
 
 	# finished step 1!
 	font.save(sys.argv[3])
