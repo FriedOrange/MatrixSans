1505bcead20d5e6a9d94c9846871a56176960fb3
Brad Neil <neilbrad2@gmail.com>
2026-09-21 19:59:49 +1000

    Split feature file to prepare for Mono features

--- sources/patchccmp.py
+++ sources/patchccmp.py
@@ -2,7 +2,7 @@
 
 import re
 
-with open("features.fea", "r", encoding="utf-8") as feature_file:
+with open("common.fea", "r", encoding="utf-8") as feature_file:
 	fea_txt = feature_file.read()
 
 video_subs = re.findall(r"sub \w+ by \w+\.alt;", re.search("ss01 {.+?} ss01;", fea_txt, flags=re.DOTALL).group(0))
@@ -11,7 +11,7 @@ fea_txt = re.sub("ss01 {.+?} ss01;", lambda m: re.sub(r"(\w+) by (\w+\.alt)", r"
 
 ccmp_end_pos = re.search("} ccmp;", fea_txt).start(0)
 
-with open("MatrixSansVideo-Regular.ufo\\features.fea", "w", encoding="utf-8") as video_feature_file:
+with open("common-video.fea", "w", encoding="utf-8") as video_feature_file:
 	video_feature_file.write(fea_txt[:ccmp_end_pos])
 	video_feature_file.writelines(video_subs)
 	video_feature_file.write(fea_txt[ccmp_end_pos:])
--- step2.bat
+++ step2.bat
@@ -8,7 +8,8 @@ for %%f in (temp\Matrix*-Regular.sfd) do (
 	sfd2ufo %%f %%~nf.ufo
 )
 for %%f in (temp\MatrixSans*-Regular.sfd) do (
-	copy features.fea %%~nf.ufo\features.fea
+	echo include(../common.fea^); > %%~nf.ufo\features.fea
+	echo include(../proportional.fea^); >> %%~nf.ufo\features.fea
 )
 
 rem Add fixed lib.plist to UFO sources
@@ -17,6 +18,9 @@ for %%f in (temp\MatrixSans*.sfd) do copy temp\lib.plist %%~nf.ufo\lib.plist
 
 rem Patch Video style feature file
 python patchccmp.py
-copy MatrixSansVideo-Regular.ufo\features.fea MatrixSansSmooth-Regular.ufo\features.fea
+echo include(../common-video.fea); > MatrixSansVideo-Regular.ufo\features.fea
+echo include(../proportional.fea); >> MatrixSansVideo-Regular.ufo\features.fea
+echo include(../common-video.fea); > MatrixSansSmooth-Regular.ufo\features.fea
+echo include(../proportional.fea); >> MatrixSansSmooth-Regular.ufo\features.fea
 
 cd ..
